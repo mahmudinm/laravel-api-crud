a15cf2b6689733de72dd619c46daae8c26ad6e95
mahmudin muttaqin <claxmanx@gmail.com>
2019-02-15 15:21:59 +0700

    implement dingo api routes

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+$api = app(Router::class);
+
+$api->version('v1', function(Router $api) {
+		// $api->get('me', 'App\Http\Controllers\Auth\RegisterController@me');
+		
+});
