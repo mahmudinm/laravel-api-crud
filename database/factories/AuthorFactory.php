7d58d30e05372462f3fb32182b598dee462a98f0
mahmudin muttaqin <claxmanx@gmail.com>
2019-02-18 11:49:33 +0700

    buat seeder

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Author::class, function (Faker $faker) {
     return [
-        //
+      'nama' => $faker->name,
+      'jenis_kelamin' => $faker->randomElement(['male', 'female']),
     ];
 });
